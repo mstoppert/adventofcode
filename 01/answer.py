87ffc10d25cf1a912ea4553334e51a94dfdaba19
Marc-André Stoppert <marc@egoserv.com>
2015-12-18 00:07:59 +0100

    Solution for Day 1

---
+++ 01/answer.py
@@ -0,0 +1,38 @@
+file = open("input.txt")
+input = file.readline()
+
+def first_star(input):
+    level = 0
+
+    for char in input:
+        if char == "(":
+            level += 1
+        elif char == ")":
+            level -=1
+        else:
+            continue
+
+    print level
+
+def second_star(input):
+    level = 0
+    position = 1
+
+    for char in input:
+        if char == "(":
+            level += 1
+        elif char == ")":
+            level -=1
+        else:
+            continue
+
+        if level == -1:
+            print position
+            break
+
+        position += 1
+
+first_star(input)
+second_star(input)
+
+
